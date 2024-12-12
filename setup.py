4a1b5a6f8977e2cf31b6d3b4a1078b4d258c621c
abhinav kumar <abhinav.mg.k@gmail.com>
2024-12-12 18:44:55 +0530

    project structure complete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name='FineTuneZephyr',
+    version='0.0.0',
+    author='Abhinav Kumar',
+    author_email='abhinav.mg.k@gmail.com',
+    # install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
